0716bbcfb81c7b3877ff990d983d2d8b2f0a70bc
jkuschner <60696513+jkuschner@users.noreply.github.com>
2026-02-12 17:29:32 -0800

    create and test textnode module

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    node1 = TextNode("Hello, World!", TextType.PLAIN)
+    node2 = TextNode("This is italic text.", TextType.ITALIC)
+    node3 = TextNode("This is bold text.", TextType.BOLD)
+    node4 = TextNode("This is code.", TextType.CODE)
+    node5 = TextNode("This is a link.", TextType.LINK, url="https://example.com")
+    node6 = TextNode("This is an image.", TextType.IMAGE, url="https://example.com/image.png")
+
+    print(node1)
+    print(node2)
+    print(node3)
+    print(node4)
+    print(node5)
+    print(node6)
+
+main()
