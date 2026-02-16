1b5fc11844c7f57442aceef036d9abf452beecf1
jkuschner <60696513+jkuschner@users.noreply.github.com>
2026-02-15 18:34:03 -0800

    add htmlnode module

---
+++ src/htmlnode.py
@@ -0,0 +1,17 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
+
+    def __repr__(self):
+        print(f"Tag: {self.tag}\nValue: {self.value}\nChildren: {self.children}\nProps: {self.props}")
+
+    def to_html(self):
+        raise NotImplementedError("to_html method must be implemented by subclasses")
+    
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        return " ".join(f'{key}="{value}"' for key, value in self.props)
