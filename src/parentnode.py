180801a057521ee007fa86dd9c041c971b5853a8
jkuschner <60696513+jkuschner@users.noreply.github.com>
2026-02-15 21:14:53 -0800

    add parentnode module

---
+++ src/parentnode.py
@@ -0,0 +1,19 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError(f"Missing tag from ParentNode: {self}")
+        if not self.children:
+            raise ValueError(f"Missing children from ParentNode: {self}")
+
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for node in self.children:
+            html += node.to_html()
+
+        html += f"</{self.tag}>"
+        return html
+        
