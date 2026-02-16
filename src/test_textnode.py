1319eb03f73fc1414dceb0c76f9ca76e1e737fcc
jkuschner <60696513+jkuschner@users.noreply.github.com>
2026-02-16 13:27:12 -0800

    add unit test for text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+from leafnode import LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +21,11 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.PLAIN)
         node2 = TextNode("This is a different text node", TextType.PLAIN)
         self.assertNotEqual(node, node2)
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
